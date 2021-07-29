0fedd270e9f4c1d326eba780585f473d11304a9a
Jake <rancho974@gmail.com>
2021-07-29 19:26:43 +0900

    header

--- libft/libft.h
+++ libft/libft.h
@@ -22,6 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 
 size_t	ft_strlcat(char *dst, char *src, size_t merge_size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
@@ -45,10 +46,10 @@ int		ft_atoi(const char *str);
 char	*ft_strdup(const char *s1);
 void	*ft_calloc(size_t count, size_t size);
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
-char	*ft_strjoin(char const *s1, char const *s2)
-char	**ft_split(char const *s, char c)
-char	*ft_strtrim(char const *s1, char const *set)
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+char	*ft_strtrim(char const *s1, char const *set);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
