8cdf0b634219ea42bfc9186dbc884783fe977791
Jake <rancho974@gmail.com>
2021-09-13 03:04:43 +0900

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -22,6 +22,13 @@ size_t	ft_word_cnt(char const *s, char c)
 	return (word_cnt);
 }
 
+void	ft_alloc_str(char *word_list, char const *s,char *begin_of_str)
+{
+	word_list = *ft_substr(s, begin_of_str, (s - begin_of_str));
+	if (*word_list == 0)
+		return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**word_list;
@@ -35,16 +42,16 @@ char	**ft_split(char const *s, char c)
 	begin_of_str = &s[0];
 	while (s)
 	{
+		// 연속으로 구분자가 주어지는 경우 공백으로 저장되는 문제
 		if (s == c)
 		{
-			*word_list = *ft_substr(word_cnt, 0, (s - begin_of_str));
-			if (*word_list == 0)
-				return (0);
+			ft_alloc_str(*word_list, s, begin_of_str);
 			word_list++;
 			begin_of_str = (s + (sizeof(s) * 1));
 		}
 		s++;
 	}
+	ft_alloc_str(*word_list, s, begin_of_str);
 	word_list = 0;
 	return (word_list);
 }
