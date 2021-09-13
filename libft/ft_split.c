84bdb2bf70287ddcc0c80c5a4982b4ed9cbd295a
Jinho Im <jim@c7r8s6.42seoul.kr>
2021-09-13 21:50:07 +0900

    split substr Makefile

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jim <jim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 17:37:51 by jim               #+#    #+#             */
-/*   Updated: 2021/09/13 17:18:19 by jim              ###   ########seoul.kr  */
+/*   Updated: 2021/09/13 21:37:34 by jim              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,60 @@
 size_t	ft_word_cnt(char const *s, char c)
 {
 	size_t	word_cnt;
+	size_t	idx;
 
 	word_cnt = 0;
-	while (s)
+	idx = 0;
+	while (s[idx])
 	{
-		if (ft_strchr(s, c) != 0)
+		if (s[idx] == c)
+			idx++;
+		else
+		{
 			word_cnt++;
-		s++;
+			while (s[idx] && s[idx] != c)
+				idx++;
+		}
 	}
-		
 	return (word_cnt);
 }
 
-void	ft_alloc_str(char *word_list, char const *s, char *begin_of_str)
+void	ft_alloc_word(char const *s, char c, char **word_list)
 {
-	word_list = ft_substr(s, begin_of_str, (s - begin_of_str));
-	if (*word_list == 0)
-		return (0);
+	size_t	idx;
+	size_t	word_idx;
+	size_t	begin_of_word;	
+
+	idx = 0;
+	word_idx = 0;
+	while (s[idx])
+	{
+		if (s[idx] == c)
+			idx++;
+		else
+		{
+			begin_of_word = idx;
+			while (s[idx] && s[idx] != c)
+				idx++;
+			word_list[word_idx] = \
+			ft_substr(s, begin_of_word, (idx - begin_of_word));
+			word_idx++;
+		}
+	}
+	word_list[word_idx] = 0;
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**word_list;
-	char	*begin_of_str;
 	size_t	word_cnt;
 
 	word_cnt = ft_word_cnt(s, c);
 	if (word_cnt == 0)
 		return (0);
-	word_list = (char **)malloc(sizeof(char) * (word_cnt + 2));
+	word_list = (char **)malloc(sizeof(char *) * (word_cnt + 1));
 	if (word_list == 0)
 		return (0);
-	begin_of_str = &s[0];
-	while (s)
-	{
-		if (*s == c)
-		{
-			ft_alloc_str(*word_list, s, begin_of_str);
-			word_list++;
-			begin_of_str = (s + (sizeof(s) * 1));
-		}
-		s++;
-	}
-	ft_alloc_str(*word_list, s, begin_of_str);
-	word_list = 0;
+	ft_alloc_word(s, c, word_list);
 	return (word_list);
 }
