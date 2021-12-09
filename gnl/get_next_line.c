938df8227b5a46ed56a9f65232f022980610ea57
Jake1152 <rancho974@gmail.com>
2021-12-09 19:33:31 +0900

    25줄제한에 맞게 자주 쓰일 코드 재모듈화 필요, 프로그램 흐름 시나리오 목적에 맞게 다시 작성할것, 어떻게하면 중복없이 자연스럽고 깔끔할지.

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jim <jim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 14:47:35 by jim               #+#    #+#             */
-/*   Updated: 2021/12/08 19:08:58 by jim              ###   ########seoul.kr  */
+/*   Updated: 2021/12/09 19:30:16 by jim              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,63 @@
 #include <stdio.h>
 
 #define BUFFER_SIZE 42
-#define FD_SIZE 256
 
 char	*get_next_line(int fd)
 {
 	static char		*static_save[FD_SIZE];
 	char			read_str[BUFFER_SIZE + 1];
 	char			*new_line_str;
-	char			*tmp;
 	int				read_str_len;
 
-	read_str_len = 0;
+	if (fd < 0 || fd >= FD_SIZE || BUFFER_SIZE <= 0)
+		return (NULL);
 	static_save[fd] = ft_strdup("");
 	if (static_save[fd] == NULL)
 		return (NULL);
 	while (TRUE)
 	{
-		// 개행확인
-		new_line_str = ft_ret_newline();
+		new_line_str = ft_ret_newline(&(static_save[fd]));
 		if (new_line_str != NULL)
 			return (new_line_str);
-		// read
 		read_str_len = read(fd, read_str, BUFFER_SIZE);
-		if (read_str_len <= 0 && static_save[fd] == "")
+		// read는 0 이하인데 static에 data가 남아 있는 경우의 처리 
+		if (read_str_len <= 0 && (static_save[fd] == "" || static_save[fd] == NULL))
 			break ;
 		read_str[read_str_len] = '\0';
-		tmp = ft_strjoin(static_save[fd], read_str);
-		static_save[fd] = tmp;
-		free(tmp);
+		// ft_save() static에 저장하는 역할 만약 실패하면 return NULL하고 종료
 	}
-	// 
-	if ((ft_strncmp(static_save[fd], "", 1) > 0) && read_str_len == 0)
+	if (static_save[fd] != "" && read_str_len == 0)
 		return (static_save[fd]);
 	return (NULL);
 }
 
-char	*ft_ret_newline(void)
+char	*ft_ret_newline(char **save)
 {
 	char	*new_line_str;
 	char	*tmp;
 	size_t	new_len;
 	size_t	save_len;
 	char	*new_line_posi;
-	extern	static_save[fd];
 
-	new_line_posi = ft_strchr(static_save[fd], '\n');
-	save_len = ft_strlen(static_save[fd]);
+	new_line_posi = ft_strchr(*save, '\n');
+	save_len = ft_strlen(*save);
+	new_line_str = NULL;
 	if (new_line_posi != NULL)
 	{
-		new_len = new_line_posi - static_save[fd] + 1;
+		new_len = new_line_posi - *save + 1;
 		new_line_str = ft_alloc(new_len + 1);
-		ft_strlcpy(new_line_str, static_save[fd], new_len);
+		ft_strlcpy(new_line_str, *save, new_len + 1);
 		tmp = ft_alloc(save_len - new_len + 1);
-		ft_strlcpy(tmp, static_save[fd] + new_len, save_len - new_len + 1);
-		free(static_save[fd]);
-		static_save[fd] = tmp;
+		if (tmp == NULL)
+			return (NULL);
+		ft_strlcpy(tmp, (*save + new_len), save_len - new_len + 1);
+		// "hello\nworld"
+		free(*save);
+		*save = tmp;
 		free(tmp);
-		free(static_save[fd]);
-		static_save[fd] = ft_strdup("");
-		printf("static_save[fd] : %s\n", static_save[fd] );
-		return (new_line_str);
+		// printf("save : %s\n", save);
 	}
+	return (new_line_str);
 }
 
 char	*ft_alloc(int size)
@@ -87,10 +83,18 @@ char	*ft_alloc(int size)
 	return (ret_str);
 }
 
-char	*ft_save(char *s1, char *s2)
+int		*ft_save(char **save, char *read_str)
 {
+	char	*tmp;
+
+	tmp = ft_strjoin(save, read_str);
+	if (tmp == NULL)
+		return (NULL);
+	*save = tmp;
+	free(tmp);
 	/*
 		s1, s2를 넘겨받아서 새로운 변수에 저장한다.
+		s1은 static
 	*/
 }
 
