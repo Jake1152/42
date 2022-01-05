dcf45a5f285d2b754f178fcc4ea64004adfccd9e
Jake1152 <rancho974@gmail.com>
2022-01-05 13:46:58 +0900

    intro

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,8 +1,27 @@
 #include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    int return_size;
-
-    return (return_size);
+	int		return_size;
+	va_list	ap;
+	/*
+		0. specifier or just string 
+		   printf("hello \nwolrd, %s hi?, nice to meet you? \n\n Hi my name is %s I'm in %d\n\n", name1, name2, num);
+		1.  just string
+			write function or putchar
+			putchar 써도 되는지 확인 필요
+		2. specifier
+			%기호가 나오면 시작
+		
+		- 필요한 것 
+		format에는 "adgdsfgdf %d %%\n\n" 같은 것만 나오는가?
+		ap는 어떻게 처리하는가?
+		printf("%d %d %d", num1, num2, num3);
+		즉 arguments들을 va_ function들을 사용하여 처리하여야하는데 어떻게 하는가?
+	*/
+	while (*format != '\0')
+	{
+		format = ;
+	}
+	return (return_size);
 }
---
+++ printf/ft_printf.h
@@ -0,0 +1,10 @@
+# ifndef _FT_PRINTF_
+# define _FT_PRINTF_
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int ft_printf(const char *format, ...);
+
+    va_list
+# endif
