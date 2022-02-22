1e87f30801c164b1e12d08b1e4b96a1490dcbac3
Jake1152 <rancho974@gmail.com>
2022-02-22 20:43:08 +0900

    usr1,2 test

--- minitalk/test/sigaction_test/sigaction_hander_00.c
+++ minitalk/test/sigaction_test/sigaction_hander_00.c
@@ -7,29 +7,35 @@ struct sigaction act_old;
 
 void sigUSR1_handler(int signo)
 {
-    printf("Test SIGUSR1\n");
+	printf("Test SIGUSR1\n");
 }
 
 void sigUSR2_handler(int signo)
 {
-    printf("Test SIGUSR2\n");
+	printf("Test SIGUSR2\n");
 }
 
 int main(void)
 {
-    struct sigaction act_usr1;
-    struct sigaction act_usr2;
-    struct sigaction act_old;
+	struct sigaction	act_usr1;
+	struct sigaction	act_usr2;
+	struct sigaction	act_old;
+	pid_t				my_pid;
 
-    act_usr1.sa_handler = sigUSR1_handler;    // signal handler assign
-    act_usr2.sa_handler = sigUSR2_handler;    // signal handler assign
+	act_usr1.sa_handler = sigUSR1_handler;    // signal handler assign
+	act_usr2.sa_handler = sigUSR2_handler;    // signal handler assign
 
-    sigaction(SIGUSR1, &act_usr1, NULL);
-    sigaction(SIGUSR2, &act_usr2, NULL);
-    while (1)
-    {
-        printf("jake's test\n");
-        sleep(1);
-    }
-    return (0);
+	sigaction(SIGUSR1, &act_usr1, NULL);
+	sigaction(SIGUSR2, &act_usr2, NULL);
+	my_pid = getpid();
+	printf("my_pid : %d\n", my_pid);
+	while (1)
+	{
+		printf("jake's test\n");
+		kill(my_pid, SIGUSR1);
+		sleep(1);
+		kill(my_pid, SIGUSR2);
+		sleep(1);
+	}
+	return (0);
 }
