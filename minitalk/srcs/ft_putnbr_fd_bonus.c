15144e66415e22c8a6ec68e622fe5d5c0fc6caf0
Jake1152 <rancho974@gmail.com>
2022-02-25 23:18:25 +0900

    makefile update

---
+++ minitalk/srcs/ft_putnbr_fd_bonus.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jake <jake@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/08 15:36:24 by jim               #+#    #+#             */
+/*   Updated: 2022/02/25 18:06:41 by jake             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "process_communication.h"
+
+static void	ft_loop(int n, int fd)
+{
+	char	ch[11];
+	int		index;
+
+	index = 0;
+	while (n >= 10)
+	{
+		ch[index] = n % 10;
+		n /= 10;
+		index++;
+	}
+	ft_putchar_fd(n + '0', fd);
+	while (index > 0)
+	{
+		ft_putchar_fd(ch[index - 1] + '0', fd);
+		index--;
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	ft_loop(n, fd);
+}
---
+++ minitalk/srcs/utils_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jake <jake@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 14:10:59 by jim               #+#    #+#             */
+/*   Updated: 2022/02/25 18:08:18 by jake             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "process_communication.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	str_len;
+
+	if (s == NULL)
+	{
+		ft_putstr_fd("There are no string.\n", 2);
+		return ;
+	}
+	str_len = ft_strlen(s);
+	write(fd, s, str_len);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (s[idx])
+		idx++;
+	return (idx);
+}
+
+int	ft_atoi(const char *str)
+{
+	int			sign;
+	long long	result;
+
+	sign = 1;
+	while (*str == '\t' || *str == '\n' || \
+	*str == '\v' || *str == '\f' || \
+	*str == '\r' || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	if (*str)
+		return (0);
+	return ((int)result * sign);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	c1;
+	unsigned char	c2;
+	size_t			i;
+
+	i = 0;
+	while (i++ < n)
+	{
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 != c2)
+		{
+			if (c1 < c2)
+				return (-1);
+			else
+				return (1);
+		}
+		if (!c1)
+			break ;
+	}
+	return (0);
+}
+
