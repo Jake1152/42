cc25f18445e351c2b84ea22c02188c0d185b063f
Jake1152 <rancho974@gmail.com>
2022-03-29 02:48:00 +0900

    add ret flag in rotate

--- push_swap/srcs/rotate.c
+++ push_swap/srcs/rotate.c
@@ -13,33 +13,41 @@
 #include "utils.h"
 #include "push_swap.h"
 
-void	rotate(t_DoublyList *stack)
+int	rotate(t_DoublyList *stack)
 {
 	t_DoublyListNode	*prevOfHeaderNode;
 
 	if (stack == NULL || stack->currentElementCount < 2)
-		return ;
+		return (FALSE);
 	prevOfHeaderNode = stack->headerNode->pRLink;
 	stack->tailerNode = stack->headerNode;
 	stack->headerNode = prevOfHeaderNode;
+	return (TRUE);
 }
 
 void	rotate_a(t_DoublyList *stack, int print_flag)
 {
-	rotate(stack);
-	if (print_flag == TRUE)
+	int	ret_flat;
+
+	ret_flat = rotate(stack);
+	if (print_flag == TRUE && ret_flat == TRUE)
 		ft_putstr("ra\n");
 }
 
 void	rotate_b(t_DoublyList *stack, int print_flag)
 {
-	rotate(stack);
-	if (print_flag == TRUE)
+	int	ret_flat;
+
+	ret_flat = rotate(stack);
+	if (print_flag == TRUE && ret_flat == TRUE)
 		ft_putstr("rb\n");
 }
 
 void	rotate_both(t_DoublyList *a_stack, t_DoublyList *b_stack)
 {
+	if ((a_stack == NULL || a_stack->currentElementCount < 2) && \
+		(b_stack == NULL || b_stack->currentElementCount < 2))
+		return ;
 	rotate_a(a_stack, FALSE);
 	rotate_b(b_stack, FALSE);
 	ft_putstr("rr\n");
