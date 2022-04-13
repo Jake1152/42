2f1bf28184b3956c44b2bab0661747132fcb7619
Jake1152 <rancho974@gmail.com>
2022-04-13 19:40:37 +0900

    start philo

---
+++ philosopher/philo/philo.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jim <jim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 19:28:39 by jim               #+#    #+#             */
+/*   Updated: 2022/04/13 19:39:19 by jim              ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "philo.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	philo_eat()
+{
+	;
+}
+
+void	philo_action()
+{
+	;
+}
+
+int main(int argc, char *argv[])
+{
+	// if (argc == 5)
+
+	return (0);
+}
---
+++ philosopher/test/pthread_create_01.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	*t_function(void *data)
+{
+	pid_t		pid;
+	pthread_t	tid;
+	char		*thread_name;
+	int			i;
+
+	pid = getpid();
+	tid = pthread_self();
+	thread_name = (char *)data;
+	i = 0;
+	while (i < 3)
+	{
+		printf("[%s] pid: %u, tid: %x --- %d\n",
+				thread_name, (unsigned int)pid, (unsigned int)tid, i);
+		i++;
+		sleep(1);
+	}
+}
+
+int	main()
+{
+	pthread_t	p_thread[2];
+	int			thr_id;
+	int			*status;
+	char		p1[] = "thread_1";
+	char		p2[] = "thread_2";
+	char		pM[] = "thread_m";
+
+	sleep(1);
+	// 1번 thread 생성
+	// t_function의 매개변수 p1으로 넘김
+	thr_id = pthread_create(&p_thread[0], NULL, t_function, (void *)p1);
+
+	// pthread_create()으로 성공적으로 thread가 생성되면 0을 return
+	if (thr_id < 0)
+	{
+		perror("thread create error : \n");
+		exit(EXIT_SUCCESS);
+	}
+
+	// 2번 thread 생성
+	thr_id = pthread_create(&p_thread[1], NULL, t_function, (void *)p2);
+
+	t_function((void *)pM);
+
+	pthread_join(p_thread[0], &status);
+	pthread_join(p_thread[1], &status);
+
+	printf("when will be end?\n");
+	return (0);
+}
