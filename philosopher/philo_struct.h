25e8673ed70861b283e9ae8b04afb18347edacec
Jake1152 <rancho974@gmail.com>
2022-04-19 19:35:59 +0900

    header update, common enum

--- philosopher/philo_struct.h
+++ philosopher/philo_struct.h
@@ -6,7 +6,7 @@
 /*   By: jim <jim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 18:05:17 by jim               #+#    #+#             */
-/*   Updated: 2022/04/18 18:22:04 by jim              ###   ########seoul.kr  */
+/*   Updated: 2022/04/19 19:31:23 by jim              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,26 @@
 
 typedef struct s_philo
 {
-	int		number_of_philosophers;
-	double	time_to_die;
-	double	time_to_eat;
-	double	time_to_sleep;
-	int		time_to_must_eat;
+	int				number_of_philosophers;
+	int				philo_back_number;
+	double			time_to_die;
+	double			time_to_eat;
+	double			time_to_sleep;
+	int				must_eat_cnt;
+	pthread_mutex_t	*forks;
 }				t_philo;
 
 typedef enum e_pthread_flag
 {
 	SUCCESS,
 }			t_pthread_flag;
+
+typedef enum e_common_flag
+{
+	FALSE,
+	TRUE,
+}			t_common_flag;
+
+
 
 #endif
